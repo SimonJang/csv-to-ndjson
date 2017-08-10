c24f757c766fc9921d05ea48574da9be4ab02c51
Simon <simon.jang.contact@gmail.com>
2017-08-10 20:18:46 +0200

    writing main logic

--- index.js
+++ index.js
@@ -1,10 +1,61 @@
 'use strict';
-module.exports = (input, opts) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
+const fs = require('fs');
+const eol = require('os').EOL;
+const csv = require('csv');
+const isValidPath = require('is-valid-path');
+
+const validateHeader = header => header.filter(x => typeof x !== 'string').length === 0;
+let getHeader = false;
+let csvHeader;
+
+module.exports = (path, options) => {
+	if (!isValidPath(path)) {
+		throw new Error('Invalid path');
 	}
 
-	opts = opts || {};
+	if (options.header) {
+		if (!Array.isArray(options.header) || options.header.length > 0) {
+			throw new Error('Invalid header argument');
+		}
 
-	return input + ' & ' + (opts.postfix || 'rainbows');
+		if (!validateHeader(options.header)) {
+			throw new Error('Header argument can only contains strings');
+		}
+
+		getHeader = true;
+		csvHeader = options.header;
+	}
+
+	if (options.destination && !isValidPath(options.destination)) {
+		throw new Error('Invalid destination path');
+	}
+
+	try {
+		const stream = fs.createReadStream(path)
+			.pipe(csv.parse())
+			.pipe(csv.transform(record => {
+				const result = {};
+
+				if (!getHeader) {
+					getHeader = true;
+					csvHeader = record;
+					return;
+				}
+
+				for (const attribute of csvHeader) {
+					result[csvHeader[attribute]] = record[attribute];
+				}
+				return JSON.stringify(result) + eol;
+			}));
+
+		if (options.destination) {
+			fs.createReadStream(stream).pipe(fs.createWriteStream(options.destination));
+			return;
+		}
+
+		return stream;
+	} catch (err) {
+		console.error(err);
+		throw new Error('Tranforming CSV to ndjson failed');
+	}
 };
--- test.js
+++ test.js
@@ -1,11 +1,6 @@
 import test from 'ava';
-import m from '.';
+// Import m from '.';
 
-test('title', t => {
-	const err = t.throws(() => {
-		m(123);
-	}, TypeError);
-	t.is(err.message, 'Expected a string, got number');
-
-	t.is(m('unicorns'), 'unicorns & rainbows');
+test('Placeholder test', t => {
+	t.true(true);
 });
